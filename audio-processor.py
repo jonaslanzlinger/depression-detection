4685156ee45d2aa94979ec54adec1dce16749fb8
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-04-03 09:12:50 +0200

    ft: add mongodb to store metrics

--- audio-processor.py
+++ audio-processor.py
@@ -1,6 +1,12 @@
 from kafka import KafkaConsumer
 import json
 from pitch import estimate_pitch
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.audio_data
+collection = db.base_frequency
 
 consumer = KafkaConsumer(
     "s1-mic1-audio",
@@ -18,3 +24,10 @@ for msg in consumer:
 
     f0 = estimate_pitch(audio, sample_rate)
     print(f"Base Frequency: {f0:.2f} Hz")
+
+    # construct metric object
+    doc = {"timestamp": datetime.datetime.utcnow(), "base_frequency": f0}
+
+    # save metric to database
+    collection.insert_one(doc)
+    print("document saved to db")
