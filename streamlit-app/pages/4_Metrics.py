3b34587b0959e01ac4b2a5d5a8629218f320b72d
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-15 20:50:29 +0200

    ft: add delete buttons for user_id and all

--- streamlit-app/pages/4_Metrics.py
+++ streamlit-app/pages/4_Metrics.py
@@ -2,15 +2,21 @@ import streamlit as st
 from pymongo import MongoClient
 import pandas as pd
 
+
+st.title("Metrics")
+
 client = MongoClient("mongodb://mongodb:27017")
 db = client["iotsensing"]
 collection = db["metrics"]
 
+if collection.count_documents({}) == 0:
+    st.warning("Please select a user in the Home tab.")
+    st.stop()
+
 df = pd.DataFrame(collection.find())
-df["date"] = pd.to_datetime(df["date"], format="%d.%m.%Y")
+df["timestamp"] = pd.to_datetime(df["timestamp"])
 selected_user = st.session_state.get("user_id", None)
 
-st.title("Metrics")
 if selected_user:
     user_df = df[df["user_id"] == selected_user]
     st.dataframe(user_df.drop(columns=["_id"]))
