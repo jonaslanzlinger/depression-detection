2ea502f9c1f65f180ca3a296a548ef50274bbd13
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-20 16:30:54 +0200

    ft: data ingestion architecture and example audio sensor

---
+++ data-ingestion/AudioSensor.py
@@ -0,0 +1,53 @@
+from IAudioDevice import IAudioDevice
+import numpy as np
+import pyaudio
+
+
+class AudioSensor(IAudioDevice):
+    def __init__(
+        self,
+        sample_rate=16000,
+        channels=1,
+        dtype="int16",
+        topic="miscellaneous",
+        mqtthostname="localhost",
+        mqttport=1883,
+    ):
+        super().__init__(
+            sample_rate=sample_rate,
+            channels=channels,
+            dtype=dtype,
+            topic=topic,
+            mqtthostname=mqtthostname,
+            mqttport=mqttport,
+        )
+        self.frame_size = 512
+        self._dtype_np = np.int16
+
+        self.pyaudio_instance = pyaudio.PyAudio()
+        self.stream = self.pyaudio_instance.open(
+            format=pyaudio.paInt16,
+            channels=self.channels,
+            rate=self.sample_rate,
+            input=True,
+            frames_per_buffer=self.frame_size,
+        )
+
+    def collect(self):
+        audio_chunk = self.stream.read(self.frame_size, exception_on_overflow=False)
+        return np.frombuffer(audio_chunk, dtype=self._dtype_np)
+
+    def filter(self, raw_data):
+        return raw_data
+
+    def transport(self, filtered_data):
+        super().transport(filtered_data)
+
+    def run(self):
+        super().run()
+
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.pyaudio_instance.terminate()
+        super().stop()
---
+++ data-ingestion/IAudioDevice.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod
+import json
+from IEdgeDevice import IEdgeDevice
+import time
+import numpy as np
+from audio_utils import encode_audio_to_base64
+
+
+class IAudioDevice(IEdgeDevice):
+    def __init__(
+        self,
+        sample_rate=16000,
+        channels=1,
+        dtype="int16",
+        topic="miscellaneous",
+        mqtthostname="localhost",
+        mqttport=1883,
+    ):
+        super().__init__(topic=topic, mqtthostname=mqtthostname, mqttport=mqttport)
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.dtype = dtype
+
+    @abstractmethod
+    def collect(self):
+        pass
+
+    @abstractmethod
+    def filter(self, raw_data):
+        pass
+
+    def transport(self, filtered_data):
+        if isinstance(filtered_data, list):
+            audio_np = np.concatenate(filtered_data)
+        else:
+            audio_np = filtered_data
+        audio_b64 = encode_audio_to_base64(audio_np, self.sample_rate)
+
+        payload = {
+            "data": audio_b64,
+            "timestamp": time.time(),
+            "sample_rate": self.sample_rate,
+        }
+
+        payload_str = json.dumps(payload)
+        result = self.client.publish(self.topic, payload_str)
+        result.wait_for_publish()
+        print("Published audio segment")
+
+    def run(self):
+        print("Started sensing. Ctrl+C to stop.")
+        try:
+            while True:
+                raw = self.collect()
+                filtered = self.filter(raw)
+                if filtered is not None:
+                    self.transport(filtered)
+                time.sleep(0.01)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        super().stop()
