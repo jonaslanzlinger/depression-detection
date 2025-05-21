e84d6b2b89784bb41a85b3a6c4f2eaa32ed61ea9
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-21 08:43:30 +0200

    ft: add VoiceFromFile implementation for IAudioDevice and as a subtype of AudioFromFile

---
+++ data-ingestion/implementations/VoiceFromFile.py
@@ -0,0 +1,71 @@
+from implementations.AudioFromFile import AudioFromFile
+import torch
+import numpy as np
+from framework.audio_utils import int2float
+
+
+class VoiceFromFile(AudioFromFile):
+    def __init__(
+        self,
+        filepath=None,
+        sample_rate=16000,
+        channels=1,
+        dtype="int16",
+        topic="miscellaneous",
+        mqtthostname="localhost",
+        mqttport=1883,
+    ):
+        super().__init__(
+            filepath=filepath,
+            sample_rate=sample_rate,
+            channels=channels,
+            dtype=dtype,
+            topic=topic,
+            mqtthostname=mqtthostname,
+            mqttport=mqttport,
+        )
+
+        model, utils = torch.hub.load(
+            repo_or_dir="snakers4/silero-vad", model="silero_vad", force_reload=False
+        )
+        (_, _, _, VADIterator, _) = utils
+        self.vad_model = model
+        self.vad_iterator = VADIterator(model)
+        self.voiced_confidences = []
+        self.buffer = []
+        self.min_frames = 30
+
+    def collect(self):
+        return super().collect()
+
+    def filter(self, raw_data):
+        confidence = 0
+
+        # skip the last chunk, if too short for silero-vad (self.frame_size = 512)
+        if raw_data is not None and len(raw_data) == self.frame_size:
+            audio_float32 = int2float(raw_data)
+            confidence = self.vad_model(
+                torch.from_numpy(audio_float32), self.sample_rate
+            ).item()
+            self.voiced_confidences.append(confidence)
+
+        if confidence > 0.5:
+            self.buffer.append(raw_data)
+            return None
+        else:
+            if len(self.buffer) >= self.min_frames:
+                speech_segment = self.buffer.copy()
+                self.buffer.clear()
+                return speech_segment
+            else:
+                self.buffer.clear()
+                return None
+
+    def transport(self, filtered_data):
+        super().transport(filtered_data)
+
+    def run(self):
+        super().run()
+
+    def stop(self):
+        super().stop()
--- data-ingestion/sense.py
+++ data-ingestion/sense.py
@@ -1,9 +1,13 @@
 from implementations.AudioSensor import AudioSensor
 from implementations.VoiceSensor import VoiceSensor
 from implementations.AudioFromFile import AudioFromFile
+from implementations.VoiceFromFile import VoiceFromFile
 
 if __name__ == "__main__":
+
     audio_sensor = AudioSensor(topic="audio/mic1")
     voice_sensor = VoiceSensor(topic="voice/mic1")
     audio_from_file = AudioFromFile(filepath="test-voice-1.wav", topic="audio/mic1")
-    audio_from_file.run()
+    voice_from_file = VoiceFromFile(filepath="test-voice-1.wav", topic="voice/mic1")
+
+    voice_from_file.run()
