84469ffb6b1266b9c71926d37862773178205e4d
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-22 12:34:15 +0200

    ref: cleanup

--- data_ingestion_layer/sense.py
+++ data_ingestion_layer/sense.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
 
     audio_sensor = AudioSensor(topic="audio/mic1")
     voice_sensor = VoiceSensor(topic="voice/mic1")
-    audio_from_file = AudioFromFile(filepath="test-voice-1.wav", topic="audio/mic1")
-    voice_from_file = VoiceFromFile(filepath="test-voice-4.wav", topic="voice/mic1")
+    audio_from_file = AudioFromFile(
+        filepath="ex_files/test-voice-1.wav", topic="audio/mic1"
+    )
+    voice_from_file = VoiceFromFile(
+        filepath="ex_files/test-voice-4.wav", topic="voice/mic1"
+    )
 
     voice_from_file.run()
