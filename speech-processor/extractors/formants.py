787dda34dca711c8a0aa4ae6e4c6f48c1447a33f
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-22 12:22:37 +0200

    ref: remove old speech-processor implementation

--- speech-processor/extractors/formants.py
+++
@@ -1,7 +0,0 @@
-def get_formant_f1_frequencies(features_LLD):
-    """
-    Compute the formant f1 frequencies using openSMILE
-    """
-    formant_f1_frequencies = features_LLD.filter(like="F2frequency_sma3nz", axis=1)
-    formant_f1_frequencies_mean = formant_f1_frequencies.mean()
-    return formant_f1_frequencies_mean.item()
--- speech-processor/extractors/shimmer.py
+++
@@ -1,2 +0,0 @@
-def get_shimmer(features_HLD):
-    return features_HLD.filter(like="shimmerLocal_sma_amean", axis=1)
