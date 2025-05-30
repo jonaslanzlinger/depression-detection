7e97bfcb88963a4b54a9363e378d0b56b7b9cc52
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-05-30 17:34:29 +0200

    ft: make the spike threshold adaptive to currently observed data points

--- temporal_context_modeling_layer/core/services/temporal_context/SpikeDampenedEMA.py
+++ temporal_context_modeling_layer/core/services/temporal_context/SpikeDampenedEMA.py
@@ -3,9 +3,9 @@ from typing import List
 
 
 class SpikeDampenedEMA(Contextualizer):
-    def __init__(self, alpha=0.13, spike_threshold=1.0, dampening_factor=0.3):
+    def __init__(self, alpha=0.13, spike_threshold_ratio=0.1, dampening_factor=0.3):
         self.alpha = alpha
-        self.spike_threshold = spike_threshold
+        self.spike_threshold_ratio = spike_threshold_ratio
         self.dampening_factor = dampening_factor
 
     def compute(self, values: List[float]) -> List[float]:
@@ -15,9 +15,10 @@ class SpikeDampenedEMA(Contextualizer):
         ema = []
         prev_ema = values[0]
         for val in values:
+            spike_threshold = self.spike_threshold_ratio * abs(val)
             delta = abs(val - prev_ema)
             update = self.alpha * (val - prev_ema)
-            if delta > self.spike_threshold:
+            if delta > spike_threshold:
                 update *= self.dampening_factor
             prev_ema += update
             ema.append(prev_ema)
