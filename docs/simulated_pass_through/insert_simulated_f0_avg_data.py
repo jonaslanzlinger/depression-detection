f675b17a5cd21239ef435d19baf7881b208778ca
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2025-06-24 11:27:27 +0200

    ref: finalize 'simulated_pass_through' section

--- docs/simulated_pass_through/insert_simulated_f0_avg_data.py
+++ docs/simulated_pass_through/insert_simulated_f0_avg_data.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from pymongo import MongoClient
+import random
 
 mongo_uri = "mongodb://localhost:27017"
 db_name = "iotsensing"
@@ -9,39 +10,51 @@ user_id = 1
 client = MongoClient(mongo_uri)
 collection = client[db_name][collection_name]
 
-start_date = datetime.now() - timedelta(days=449)
+# clear data in collection
+collection.delete_many({"user_id": user_id, "metric_name": "f0_avg"})
 
+# 18 months
+total_days = 540
+start_date = datetime.now() - timedelta(days=total_days)
 records = []
 
-for i in range(30):
+period1 = 180
+period2 = 180
+period3 = 180
+
+# generate sample data:
+# period1: baseline behavior
+# period2: depressed behavior
+# period3: recovery behavior
+for i in range(period1):
     records.append(
         {
             "user_id": user_id,
             "timestamp": start_date + timedelta(days=i),
             "metric_name": "f0_avg",
-            "metric_value": 110.0,
+            "metric_value": round(random.normalvariate(148.091, 3), 2),
         }
     )
 
-for i in range(30, 210):
+for i in range(period1, period1 + period2):
     records.append(
         {
             "user_id": user_id,
             "timestamp": start_date + timedelta(days=i),
             "metric_name": "f0_avg",
-            "metric_value": 160.0,
+            "metric_value": round(random.normalvariate(90, 2), 2),
         }
     )
 
-for i in range(210, 450):
+for i in range(period1 + period2, total_days):
     records.append(
         {
             "user_id": user_id,
             "timestamp": start_date + timedelta(days=i),
             "metric_name": "f0_avg",
-            "metric_value": 110.0,
+            "metric_value": round(random.normalvariate(148.091, 3), 2),
         }
     )
 
 collection.insert_many(records)
-print(f"Inserted {len(records)} metric records into MongoDB.")
+print(f"Inserted {len(records)} f0_avg metric records into MongoDB.")
